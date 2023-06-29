805e9afc996163ab6431924cd21c2da6c62c0e7a
NicolaAdell <81983008+NicolaAdell@users.noreply.github.com>
2023-06-29 16:20:15 +0200

    Update control.h

--- control.h
+++ control.h
@@ -10,3 +10,21 @@ static inline float control_proporcional(float error) {
   // Devolver el porcentaje de ciclo de actividad
   return k * error;
 }
+
+/**
+ * @brief Verifica que el valor de control no exceda el maximo
+ * valor posible de PWM
+ * @param duty_ticks cantidad de ticks del duty de PWM para corregir el error
+ * @param duty_max_ticks maxima cantidad de ticks del duty de PWM
+ * @return valor final de ticks de PWM entre mas menos duty_max_ticks
+ */
+static inline int16_t saturador(float duty_ticks, int16_t duty_max_ticks) {
+  // Variable para el valor final de ticks
+  int16_t ticks = (int16_t) duty_ticks;
+  // Verifico que los ticks no superen el maximo
+  if(duty_ticks > duty_max_ticks) { ticks = duty_max_ticks; }
+  // Verifico que los ticks no superen el minimo
+  else if(duty_ticks < (-1 * duty_max_ticks)) { ticks = -1 * duty_max_ticks; }
+  // Devuelvo el valor de ticks
+  return ticks;
+}
