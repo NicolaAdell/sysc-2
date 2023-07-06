e16ab889eebf48923d4d177afea926620d849670
NicolaAdell <81983008+NicolaAdell@users.noreply.github.com>
2023-07-06 10:23:02 -0300

    Update control.h

--- control.h
+++ control.h
@@ -6,7 +6,7 @@
  */
 static inline float control_proporcional(float error) {
   // Constante de proporcionalidad
-  const float k = 1.0;
+  const float k = 1000.0;
   // Devolver el porcentaje de ciclo de actividad
   return k * error;
 }
